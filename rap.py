a5f0f7b190a4afa86bb9986ffbecd4fcb2b3a90e
sai-prathyusha-bhupathi <saiprathyushabhupathi1526@gmail.com>
2021-03-22 15:52:48 +0530

    printing

--- rap.py
+++ rap.py
@@ -1,6 +1,6 @@
-string1=input("Enter a string u want to enter in pattern:")
+
 n=int(input("Enter number of lines"))
 for i in range(0,n,1):
     for j in range(0,i,1):
-        print(string,end=" ")
+        print("*",end=" ")
     print()
