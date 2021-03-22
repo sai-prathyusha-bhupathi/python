63e3c3100fbeb28c8b8337b57b95f0ae39edd357
sai-prathyusha-bhupathi <saiprathyushabhupathi1526@gmail.com>
2021-03-22 16:45:58 +0530

    printing

--- rap.py
+++ rap.py
@@ -1,6 +1,8 @@
-
+import time
+str=input("Enter string you want to print")
 n=int(input("Enter number of lines"))
 for i in range(0,n,1):
     for j in range(0,i+1,1):
-        print("*",end=" ")
+        print(str, end=" ")
     print()
+time.sleep(30)
